50026e78a43e6169ca546a8be8d7e4539e99ac24
vasisht54 <vasisht.theviper@gmail.com>
2020-10-03 18:30:51 -0400

    Set up custom fonts

--- App.js
+++ App.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Home from './screens/home';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+/* const getFonts = () => {
+  return Font.loadAsync({
+    'galdeano-regular': require('./assets/fonts/Galdeano-Regular.ttf'),
+    'crimsonText-regular': require('./assets/fonts/CrimsonText-Regular.ttf'),
+    'crimsonText-bold': require('./assets/fonts/CrimsonText-Bold.ttf'),
+  })
+} */
 
 export default function App() {
-  return (
-    <Home />
-  );
+  let [fontsLoaded] = Font.useFonts({
+    'galdeano-regular': require('./assets/fonts/Galdeano-Regular.ttf'),
+    'crimsonText-regular': require('./assets/fonts/CrimsonText-Regular.ttf'),
+    'crimsonText-bold': require('./assets/fonts/CrimsonText-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  else {
+    return <Home />;
+  }
+
+  /* const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      <Home />
+    )
+  } else {
+    return (
+      <AppLoading 
+        startAsync = {getFonts}
+        onFinish = {() => setFontsLoaded(true)}
+      />
+    )
+  } */
 }
